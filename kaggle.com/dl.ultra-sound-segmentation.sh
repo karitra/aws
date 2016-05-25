56fbfaee54935f1215ef8f88341b47fbcaa7a643
karitra <karev.alexandre@gmail.com>
2016-05-25 19:20:35 +0300

    kaggle dl script update

--- kaggle.com/dl.ultra-sound-segmentation.sh
+++ kaggle.com/dl.ultra-sound-segmentation.sh
@@ -7,7 +7,7 @@ pushd $WORKING_PATH
 
 echo Downloading data...
 wget -q https://www.kaggle.com/c/ultrasound-nerve-segmentation/download/train_masks.csv.zip &
-sleep 5 wget -q https://www.kaggle.com/c/ultrasound-nerve-segmentation/download/sample_submission.csv.zip & 
+sleep 5 && wget -q https://www.kaggle.com/c/ultrasound-nerve-segmentation/download/sample_submission.csv.zip & 
 sleep 5 && wget -q https://www.kaggle.com/c/ultrasound-nerve-segmentation/download/test.zip &
 sleep 10 && wget -q https://www.kaggle.com/c/ultrasound-nerve-segmentation/download/train.zip &
 
