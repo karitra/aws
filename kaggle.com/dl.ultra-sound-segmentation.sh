223d4cf67e8ea067039219f216692d11cdb6430b
karitra <karev.alexandre@gmail.com>
2016-05-25 20:34:37 +0300

    kaggle dl uns cookies

--- kaggle.com/dl.ultra-sound-segmentation.sh
+++ kaggle.com/dl.ultra-sound-segmentation.sh
@@ -5,6 +5,11 @@ WORKING_PATH=$HOME/data/kaggle.com/UNS
 mkdir -p $WORKING_PATH
 pushd $WORKING_PATH
 
+#
+# TODO: error report
+#
+cp cookies.txt $WORKING_PATH
+
 echo Downloading data...
 wget -q -nH https://www.kaggle.com/c/ultrasound-nerve-segmentation/download/train_masks.csv.zip &
 wget -q --load-cookies cookies.txt -nH https://www.kaggle.com/c/ultrasound-nerve-segmentation/download/sample_submission.csv.zip
